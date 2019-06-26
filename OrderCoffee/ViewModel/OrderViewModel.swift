dc7e241f4df7ffb64b13edcf7a4b8aea03ae5dc3
Piyush Vaish <piyush.vaish@gmail.com>
2019-06-26 19:26:17 +0530

    ViewModel done for OrderListView

---
+++ OrderCoffee/ViewModel/OrderViewModel.swift
@@ -0,0 +1,41 @@
+//
+//  OrderViewModel.swift
+//  OrderCoffee
+//
+//  Created by Piyush Vaish on 26/06/19.
+//  Copyright © 2019 Piyush Vaish. All rights reserved.
+//
+
+import Foundation
+
+class OrderListViewModel{
+    var orderViewModel : [OrderViewModel]
+    init() {
+        self.orderViewModel = [OrderViewModel]()
+    }
+}
+
+extension OrderListViewModel{
+    func orderViewModel (at index: Int) ->OrderViewModel{
+        return self.orderViewModel[index]
+    }
+}
+
+struct OrderViewModel{
+    var order : Order
+}
+
+extension OrderViewModel{
+    var name : String{
+        return self.order.name
+    }
+    var email : String{
+        return self.order.email
+    }
+    var type : String{
+        return self.order.type.rawValue.capitalized
+    }
+    var size : String{
+        return self.order.size.rawValue.capitalized
+    }
+}
